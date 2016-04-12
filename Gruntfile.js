23f9e41b5fa28f5b2a2fb1bd786f2ac56d922f45
Engebretsen, Hans <Hans.Engebretsen@garmin.com>
2016-04-12 09:19:50 -0500

    removed ruby sass in favor of lib sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -128,7 +128,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
